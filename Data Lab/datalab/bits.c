50a05f822e0e49884b8f568847bc071deb13b926
shirleychen920 <chenhaojie94@gmail.com>
2015-11-29 19:14:08 -0800

    CS33 LABS

---
+++ Data Lab/datalab/bits.c
@@ -0,0 +1,266 @@
+/*
+ * CS:APP Data Lab
+ *
+ * <Haojie Chen 204449491>
+ *
+ * bits.c - Source file with your solutions to the Lab.
+ *          This is the file you will hand in to your instructor.
+ *
+ * WARNING: Do not include the <stdio.h> header; it confuses the dlc
+ * compiler. You can still use printf for debugging without including
+ * <stdio.h>, although you might get a compiler warning. In general,
+ * it's not good practice to ignore compiler warnings, but in this
+ * case it's OK.
+ */
+
+#if 0
+/*
+ * Instructions to Students:
+ *
+ * STEP 1: Read the following instructions carefully.
+ */
+
+You will provide your solution to the Data Lab by
+editing the collection of functions in this source file.
+
+INTEGER CODING RULES:
+
+  Replace the "return" statement in each function with one
+  or more lines of C code that implements the function. Your code
+  must conform to the following style:
+
+  int Funct(arg1, arg2, ...) {
+      /* brief description of how your implementation works */
+      int var1 = Expr1;
+      ...
+      int varM = ExprM;
+
+      varJ = ExprJ;
+      ...
+      varN = ExprN;
+      return ExprR;
+  }
+
+  Each "Expr" is an expression using ONLY the following:
+  1. Integer constants 0 through 255 (0xFF), inclusive. You are
+      not allowed to use big constants such as 0xffffffff.
+  2. Function arguments and local variables (no global variables).
+  3. Unary integer operations ! ~
+  4. Binary integer operations & ^ | + << >>
+
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
+
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting.
+  7. Use any data type other than int.  This implies that you
+     cannot use arrays, structs, or unions.
+
+
+  You may assume that your machine:
+  1. Uses 2s complement, 32-bit representations of integers.
+  2. Performs right shifts arithmetically.
+  3. Has unpredictable behavior when shifting an integer by more
+     than the word size.
+
+EXAMPLES OF ACCEPTABLE CODING STYLE:
+  /*
+   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+   */
+  int pow2plus1(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     return (1 << x) + 1;
+  }
+
+  /*
+   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+   */
+  int pow2plus4(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     int result = (1 << x);
+     result += 4;
+     return result;
+  }
+
+FLOATING POINT CODING RULES
+
+For the problems that require you to implent floating-point operations,
+the coding rules are less strict.  You are allowed to use looping and
+conditional control.  You are allowed to use both ints and unsigneds.
+You can use arbitrary integer and unsigned constants.
+
+You are expressly forbidden to:
+  1. Define or use any macros.
+  2. Define any additional functions in this file.
+  3. Call any functions.
+  4. Use any form of casting.
+  5. Use any data type other than int or unsigned.  This means that you
+     cannot use arrays, structs, or unions.
+  6. Use any floating point data types, operations, or constants.
+
+
+NOTES:
+  1. Use the dlc (data lab checker) compiler (described in the handout) to
+     check the legality of your solutions.
+  2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
+     that you are allowed to use for your implementation of the function.
+     The max operator count is checked by dlc. Note that '=' is not
+     counted; you may use as many of these as you want without penalty.
+  3. Use the btest test harness to check your functions for correctness.
+  4. Use the BDD checker to formally verify your functions
+  5. The maximum number of ops for each function is given in the
+     header comment for each function. If there are any inconsistencies
+     between the maximum ops in the writeup and in this file, consider
+     this file the authoritative source.
+
+/*
+ * STEP 2: Modify the following functions according the coding rules.
+ *
+ *   IMPORTANT. TO AVOID GRADING SURPRISES:
+ *   1. Use the dlc compiler to check that your solutions conform
+ *      to the coding rules.
+ *   2. Use the BDD checker to formally verify that your solutions produce
+ *      the correct answers.
+ */
+
+
+#endif
+/* howManyBits - return the minimum number of bits required to represent x in
+ *             two's complement
+ *  Examples: howManyBits(12) = 5
+ *            howManyBits(298) = 10
+ *            howManyBits(-5) = 4
+ *            howManyBits(0)  = 1
+ *            howManyBits(-1) = 1
+ *            howManyBits(0x80000000) = 32
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 90
+ *  Rating: 4
+ */
+int howManyBits(int x) {
+
+       int copy, case0, case_n1, half8, half4, half2, half1, half16, shift16, shift8, shift4, shift2, shift1, counter;
+       copy = x ^ (x >> 31);     //If x is positive, copy is the same, if x is negative, negate it
+       // case 0 or -1, return 1
+       case0 = !x;
+       case_n1 = !(x+1);
+
+       half16 = copy >> 16;
+       shift16 = !(!half16) << 4;  //If the left half are zeros, shift 16 is 0 and copy will not shift; if the left half is not zero, shift 16 is 16 and copy will shift 16 bits to the right; shift 16 also serves as a counter
+       copy = copy >> shift16;
+
+       half8 = copy >> 8;
+       shift8 = !(!half8) << 3;
+       copy = copy >> shift8;
+
+       half4 = copy >> 4;
+       shift4 = !(!half4) << 2;
+       copy = copy >> shift4;
+
+       half2 = copy >> 2;
+       shift2 = !(!half2) << 1;
+       copy = copy >> shift2;
+
+       half1 = copy >> 1;
+       shift1 = !(!half1);
+
+       counter = shift16 + shift8 + shift4 + shift2 + shift1 + 2; //2 accounts for the sign bit and the bit that we didn't count in the end
+
+       return (case0 | case_n1 | (counter & ~(~case0 + 1) & ~(~case_n1 + 1)));
+
+  
+}
+/*
+ * sm2tc - Convert from sign-magnitude to two's complement
+ *   where the MSB is the sign bit
+ *   Example: sm2tc(0x80000005) = -5.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 4
+ */
+int sm2tc(int x) {
+  int m, magnitude;
+  m = x >> 31;   //description of sign bit
+  magnitude = ~(~0 << 31) & x;
+  return ((~magnitude + 1) & m) + (x & ~m);
+}
+/*
+ * isNonNegative - return 1 if x >= 0, return 0 otherwise
+ *   Example: isNonNegative(-1) = 0.  isNonNegative(0) = 1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 3
+ */
+int isNonNegative(int x) {
+  int sign_bit;
+  sign_bit = x >> 31;
+  return (sign_bit + 1);
+}
+
+/*
+ * rotateRight - Rotate x to the right by n
+ *   Can assume that 0 <= n <= 31
+ *   Examples: rotateRight(0x87654321,4) = 0x76543218
+ *   Legal ops: ~ & ^ | + << >>
+ *   Max ops: 25
+ *   Rating: 3
+ */
+int rotateRight(int x, int n) {
+  int rotate_bits, rest;
+  rotate_bits = x << (32 + (~n + 1));
+  rest =  (x >> n) & ~(~0 << (32 + (~n + 1)));
+  return rotate_bits | rest;
+
+}
+/*
+ * divpwr2 - Compute x/(2^n), for 0 <= n <= 30
+ *  Round toward zero
+ *   Examples: divpwr2(15,1) = 7, divpwr2(-33,4) = -2
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 2
+ */
+int divpwr2(int x, int n) {
+  int bias;
+  bias = (x >> 31) & ((1 << n) + ~0);     //if x is negative, x >> 31 is 0xFFFFFFFF, and it will return the value of (2^n -1) to bias
+  return (x + bias) >> n;
+}
+/*
+ * allOddBits - return 1 if all odd-numbered bits in word set to 1
+ *   Examples allOddBits(0xFFFFFFFD) = 0, allOddBits(0xAAAAAAAA) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 2
+ */
+ int allOddBits(int x) {
+   int m1, m2;
+   m1 = ~(0x55 << 24);
+   m2 = m1 & (m1 >> 8) & (m1 >> 16) & (m1 >> 24); 
+   return !((x & m2) ^ m2);
+}
+/*
+ * bitXor - x^y using only ~ and &
+ *   Example: bitXor(4, 5) = 1
+ *   Legal ops: ~ &
+ *   Max ops: 14
+ *   Rating: 1
+ */
+int bitXor(int x, int y) {
+  return (~(x & y)) & (~(~x & ~y));
+}
+/*
+ * isTmin - returns 1 if x is the minimum, two's complement number,
+ *     and 0 otherwise
+ *   Legal ops: ! ~ & ^ | +
+ *   Max ops: 10
+ *   Rating: 1
+ */
+int isTmin(int x) {
+  return !(~x + 1 + ~x + 1 + !x);
+}
---
+++ Smashing Lab/inject.s
@@ -0,0 +1,6 @@
+lea -0x9B(%rsp), %rdi
+mov $0x57, %rax
+syscall	
+
+	
+	
